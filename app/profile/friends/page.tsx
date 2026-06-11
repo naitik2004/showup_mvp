4590d38fda84be4db17ce9b4a8225d58983583a1
naitik2004 <naitik123456789pandey@gmail.com>
2026-06-11 23:29:27 +0530

    fix: add valid friends page

---
+++ app/profile/friends/page.tsx
@@ -0,0 +1,7 @@
+export default function FriendsPage() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      Friends Page Coming Soon
+    </div>
+  );
+}
